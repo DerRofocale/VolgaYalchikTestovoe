981fe2f59345f6d04df7f6823153e6960653a4ef
DerRofocale <dmitry_yalchik@mail.ru>
2021-10-06 19:44:54 +0500

    With Log class

--- VolgaYalchikTestovoe/Program.cs
+++ VolgaYalchikTestovoe/Program.cs
@@ -11,11 +11,51 @@ namespace VolgaYalchikTestovoe
         {
             var aboba = new List<string>();
             char[] sep = { ' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t' };
-            string text;
-            Console.WriteLine("Введите полный путь до текстового файла с его названием и расширением...");
-            string path = Console.ReadLine();
-            StreamReader sr = new StreamReader(path);
-            text = sr.ReadToEnd();
+            string text = "";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Введи полный путь до текстового файла с его названием.\n" +
+                "Также можешь ввести название файла, если он хранится на твоём рабочем столе.");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("При обоих вариантах ввода не указывай расширение файла.");
+            Console.ResetColor();
+
+            string temp_path;
+            string path;
+            while (true)
+            {
+                temp_path = Console.ReadLine();
+                try
+                {
+                    if (temp_path.Contains('/') || temp_path.Contains('\\'))
+                    {
+                        path = temp_path + ".txt";
+                    }
+                    else
+                    {
+                        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                        path = desktopPath + "\\" + temp_path + ".txt";
+                    }
+                    StreamReader sr = new StreamReader(path);
+                    text = sr.ReadToEnd();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Файл успешно загружен. \nНачинаю обрабатывать данные.");
+                    Console.ResetColor();
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Log("Файл не найден или некорректное расширение файла. Используйте только файлы с расширением \".txt\". " +
+                        "Сообщение ошибки: " + ex.Message + "\n");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Упс... Произошла ошибка, проверь введённые данные и отправь корректный путь.");
+                    Console.ResetColor();
+                    continue;
+                }
+            }
+
             string[] words;
             words = text.ToUpper().Split(sep);
             int count_mass = words.Length;
@@ -50,7 +90,19 @@ namespace VolgaYalchikTestovoe
                                 continue;
                             }
                         }
-                        Count.Add(words[i], temp_counter);
+                        try
+                        {
+                            Count.Add(words[i], temp_counter);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log("Невозможно добавить слово в список." +
+                            "Сообщение ошибки: " + ex.Message + "\n");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Ой-ой-ой... Увы, я, по непонятной для меня причине, не могу добавить слово в спиок.");
+                            Console.ResetColor();
+                            continue;
+                        }
                     }
                     
                 }
@@ -61,6 +113,10 @@ namespace VolgaYalchikTestovoe
             {
                 Console.WriteLine("{0} — {1}", pair.Key, pair.Value);
             }
+        }
+        public static void Log(string message)
+        {
+            File.AppendAllText("log.txt","[ " + DateTime.Now.ToLongTimeString() + " | " + DateTime.Now.ToShortDateString() + " ] " + message + "\n");
         }
     }
 }
